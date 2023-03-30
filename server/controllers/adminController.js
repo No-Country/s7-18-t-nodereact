461fac7d6ccfc96ff760d1abb110ff4654e34d33
martaalcazarr <martaalcazarredondo@gmail.com>
2023-03-29 22:59:51 -0300

    controllers y envio de mails administrador

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,28 +1,28 @@
-import User from "../models/User.js"
+import Admin from "../models/Admin.js"
 import generateJWT from "../helpers/generateJWT.js";
 import generateId from "../helpers/generateId.js";
-import registerEmail from "../helpers/emailRegister.js";
-import emailNewPassword from "../helpers/forgottenPasswordEmail.js";
+import registerEmailAdmin from "../helpers/emailRegisterAdmin";
+import emailNewPasswordAdmin from "../helpers/forgottenPasswordEmailAdmin.js";
 
-const registerUser = async(req, res) => {
+const registerAdmin = async(req, res) => {
     const {email, name } = req.body;
-    const existUser = await User.findOne({email});
+    const existAdmin = await Admin.findOne({email});
 
-    if(existUser){
-        const error = new Error("Usuario ya registrado");
+    if(existAdmin){
+        const error = new Error("Administrador ya registrado");
         return res.status(400).json({msg: error.message});
     }
 
     try {
-        const user = new User(req.body);
-        const savedUser = await user.save();
+        const admin = new Admin(req.body);
+        const savedAdmin = await admin.save();
 
-        registerEmail({
+        registerEmailAdmin({
             email, 
             name,
-            token: savedUser.token
+            token: savedAdmin.token
         });
-        res.json(savedUser);
+        res.json(savedAdmin);
         
     } catch (error) {
         console.log(error)
@@ -30,42 +30,42 @@ const registerUser = async(req, res) => {
         
     }
 
-const userProfile = async (req, res) => {
+const adminProfile = async (req, res) => {
     try {
-        const profile = await User.findbyIdI(req.params.id).lean();
+        const profile = await Admin.findbyIdI(req.params.id).lean();
         res.json(profile);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
-const confirmUser = async (req, res) => {
+const confirmAdmin= async (req, res) => {
     const {token} = req.params;
-    const userConfirm = await User.findOne({token});
+    const adminConfirm = await Admin.findOne({token});
 
-    if(!userConfirm){
+    if(!adminConfirm){
         const error = new Error("Token no válido");
         return res.status(400).json({msg: error.message});
     }
 
     try {
-        userConfirm.token = null;
-        userConfirm.confirmed = true;
-        await userConfirm.save();
+        adminConfirm.token = null;
+        adminConfirm.confirmed = true;
+        await adminConfirm.save();
         res.json({msg: "Usuario confirmado correctamente"})
     } catch (error) {
         console.log(error)
     }
 }
 
-const authenticateUser = async (req, res) => {
+const authenticateAdmin = async (req, res) => {
     const {email, password} = req.body;
-    const user = await User.findOne({email});
+    const user = await Admin.findOne({email});
     if(!user){
-        const error = new Error("El usuario no existe");
+        const error = new Error("El administrador no existe");
         return res.status(403).json({msg: error.message});
     }
-    if(await user.checkUserPassword(password)){
+    if(await admin.checkAdminPassword(password)){
         res.json({token: generateJWT(user.id)});
     }else{
         const error = new Error("La contraseña es incorrecta");
@@ -75,18 +75,18 @@ const authenticateUser = async (req, res) => {
 
 const forgottenPassword = async (req, res) => {
     const {email} = req.body;
-    const existUser = await User.findOne({email});
-    if(!existUser){
-        const error = new Error("El usuario no existe");
+    const existAdmin = await Admin.findOne({email});
+    if(!existAdmin){
+        const error = new Error("El administrador no existe");
         return res.status(400).json({ msg: error.message });
     }
     try {
-        existUser.token = generateId();
-        await existUser.save();
-        emailNewPassword({
+        existAdmin.token = generateId();
+        await existAdmin.save();
+        emailNewPasswordAdmin({
             email,
-            name: existUser.name,
-            token: existUser.token
+            name: existAdmin.name,
+            token: existAdmin.token
         })
         res.json({
             msg: "Se ha enviado un email con las instrucciones para cambiar la contraseña"
@@ -96,29 +96,29 @@ const forgottenPassword = async (req, res) => {
     }
 }
 
-const checkUserToken = async (req, res) => {
+const checkAdminToken = async (req, res) => {
     const {token} = req.params;
-    const validToken = await User.findOne({token});
+    const validToken = await Admin.findOne({token});
     if(validToken){
-        res.json({msg: "Token válido, el usuario existe"});
+        res.json({msg: "Token válido, el administrador existe"});
     }else{
         const error = new Error("Token no válido");
         return res.status(400).json({msg: error.message});
     }
 }
 
-const newUserPassword = async (req, res) => {
+const newAdminPassword = async (req, res) => {
     const {token} = req.params;
     const {password} = req.body;
-    const user = await User.findOne({token});
-    if(!user){
+    const admin = await Admin.findOne({token});
+    if(!admin){
         const error = new Error("Hubo un error");
         res.status(400).json({ msg: error.message });
     }
     try {
-        user.token = null;
-        user.password = password;
-        await user.save();
+        admin.token = null;
+        admin.password = password;
+        await admin.save();
         res.json({msg: "Password modificado correctamente"});
     } catch (error) {
         console.log(error)
@@ -126,11 +126,11 @@ const newUserPassword = async (req, res) => {
 }
 
 export {
-    registerUser,
-    userProfile,
-    confirmUser,
-    authenticateUser,
+    registerAdmin,
+    adminProfile,
+    confirmAdmin,
+    authenticateAdmin,
     forgottenPassword,
-    checkUserToken,
-    newUserPassword,
+    checkAdminToken,
+    newAdminPassword,
 }
