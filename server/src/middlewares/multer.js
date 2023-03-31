49c748d7f0404625077c2892af05052f82a72c6c
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2023-03-30 23:06:45 -0500

    REFACTOR (BACK): Se añadió el middleware para la subida e imágenes, se exportó por defecto "upload".

--- server/src/middlewares/multer.js
+++ server/src/middlewares/multer.js
@@ -7,17 +7,19 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
     }
-    });
+});
 
-    const fileFilter = (req, file, cb) => {
+const fileFilter = (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
         cb(null, true);
     } else {
         cb(null, false);
     }
-    };
+};
 
-    const upload = multer({
+const upload = multer({
     storage: storage,
     fileFilter: fileFilter
 });
+
+export default upload;
--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {createPost} from '../controllers/postController.js';
+import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.post('/posts',createPost);
+router.post('/posts',upload.array('images'), createPost);
 
 
 
