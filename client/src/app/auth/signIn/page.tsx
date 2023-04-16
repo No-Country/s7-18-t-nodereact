042831ea08ec63e625023380c39a0d4e80bf3e7c
76luc <santamaria.luc76@gmail.com>
2023-04-16 14:57:38 -0300

    update create new recipe & login

--- client/src/app/auth/signIn/page.tsx
+++ client/src/app/auth/signIn/page.tsx
@@ -42,52 +42,37 @@ const LoginPage = () => {
   });
 
   const loginUser = async ({ email, password }) => {
-    const res = await signIn('credentials', {
-      redirect: false,
+    const { data } = await axios.post(`/users/authenticate`, {
       email,
       password,
-      callbackUrl: `${window.location.origin}`,
     });
-
-    res?.ok ? router.push('/home') : console.log(res?.error);
-
-    /*  const {
-      data: { name, email, imgAvatar, token },
-    } = await axios.post(`/users/authenticate`, {
-      email: data?.email,
-      password: data?.password,
-    });
-
+    localStorage.setItem('token', JSON.stringify(data.token));
     if (data) {
-      await signIn('credentials', {
+      const res = await signIn('credentials', {
         redirect: false,
-        email,
-        imgAvatar,
+        email: data.email,
+        name: data.name,
+        image: data.imgAvatar,
         callbackUrl: `${window.location.origin}`,
       });
-      const user: IUser = {
-        authenticated: true,
-        user: {
-          name,
-          email,
-          image: imgAvatar,
-        },
-        token,
-      };
-      dispatch(setLogin(user));
-      router.push('/home');
-    } else {
-      console.log('Error');
-    } */
+      const { data: profile } = await axios.get(`/users/profile/${data.id}`, {
+        headers: { authorization: `Bearer ${data.token}` },
+      });
+      const { token, password, ...restData } = profile;
+      dispatch(setLogin({ token, user: restData }));
+      res?.ok ? router.push('/home') : console.log(res?.error);
+    }
   };
 
-  const registerUser = async ({ email, password, ...restData }) => {
+  const registerUser = async ({ email, password, name, location }) => {
     try {
       const user = await axios.post(`/users/register`, {
         email,
         password,
-        ...restData,
+        name,
+        location,
       });
+      console.log('Register => ', user);
 
       loginUser({
         email,
@@ -101,95 +86,99 @@ const LoginPage = () => {
   const onSubmit = (data: IForm) => (isLogin ? loginUser(data) : registerUser(data));
 
   return (
-    <div className='flex flex-col justify-center items-center w-screen h-screen px-7 py-4 bg-white mx-auto md:grid grid-cols-2'>
-      <h2 className='text-4xl mb-10 w-full text-center'>Logo</h2>
-      <div className='flex flex-col justify-center items-center gap-2 w-full sm:w-3/4 md:w-full lg:w-3/4 xl:w-3/5'>
-        <h4 className='text-xl md:text-3xl'>{`${isLogin ? '¡Qué bueno conocerte!' : 'Completá tus datos '}`}</h4>
-        <form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit(onSubmit)}>
-          {isLogin ? (
-            <Button
-              onClick={() => signIn('google', { callbackUrl: '/home' })}
-              className='bg-none shadow-md w-full border border-gray-300 hover:bg-gradient-to-r from-[#FF8C00] to-[#FFD700] flex justify-center items-center gap-3'
-            >
-              <Image width={40} height={40} src='/google-logo.png' alt='Google logo' />
-              <p className='text-black text-lg'>Continuar con Google</p>
-            </Button>
-          ) : (
+    <div className='flex flex-col justify-center items-center w-screen h-screen px-7 py-4 bg-white mx-auto md:grid grid-cols-2 md:p-0'>
+      <div className='hidden md:flex justify-center items-center w-full h-full bg-gradient-to-r from-[#ff823f] to-[#ffd700]'>
+        <Image width={500} height={500} src='/img/svg/logo.svg' alt='logo' />
+      </div>
+      <div className='flex justify-center p-5 w-full h-full'>
+        <div className='flex flex-col justify-center items-center gap-2 w-full sm:w-3/4 md:w-full lg:w-3/4 xl:w-3/'>
+          <h4 className='text-xl md:text-3xl'>{`${isLogin ? '¡Qué bueno conocerte!' : 'Completá tus datos '}`}</h4>
+          <form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit(onSubmit)}>
+            {isLogin ? (
+              <Button
+                onClick={() => signIn('google', { callbackUrl: '/home' })}
+                className='bg-none shadow-md w-full border border-gray-300 hover:bg-gradient-to-r from-[#FF8C00] to-[#FFD700] flex justify-center items-center gap-3'
+              >
+                <Image width={40} height={40} src='/google-logo.png' alt='Google logo' />
+                <p className='text-black text-lg'>Continuar con Google</p>
+              </Button>
+            ) : (
+              <div className='flex flex-col gap-3'>
+                <div>
+                  <input
+                    {...register('name')}
+                    autoComplete='on'
+                    placeholder='Nombre'
+                    className={`textbox h-[48px] ${!errors.name && 'focus:shadow-blue-500 focus:border-blue-500'} ${
+                      errors.name && 'border-red-500 shadow-red-500'
+                    }`}
+                  />
+                  {errors.name && <p className='text-xs text-red-500 mt-1'>{errors.name?.message}</p>}
+                </div>
+                <input
+                  {...register('location')}
+                  autoComplete='on'
+                  placeholder='País'
+                  className='textbox h-[48px] focus:shadow-blue-500 focus:border-blue-500'
+                />
+              </div>
+            )}
+            <Divider />
+
             <div className='flex flex-col gap-3'>
               <div>
                 <input
-                  {...register('name')}
+                  {...register('email')}
                   autoComplete='on'
-                  placeholder='Nombre'
-                  className={`textbox h-[48px] ${!errors.name && 'focus:shadow-blue-500 focus:border-blue-500'} ${
-                    errors.name && 'border-red-500 shadow-red-500'
+                  placeholder='Correo electrónico'
+                  className={`textbox h-[48px] ${!errors.email && 'focus:shadow-blue-500 focus:border-blue-500'} ${
+                    errors.email && 'border-red-500 shadow-red-500'
                   }`}
                 />
-                {errors.name && <p className='text-xs text-red-500 mt-1'>{errors.name?.message}</p>}
+                {errors.email && <p className='text-xs text-red-500 mt-1'>{errors.email?.message}</p>}
               </div>
-              <input
-                {...register('location')}
-                autoComplete='on'
-                placeholder='País'
-                className='textbox h-[48px] focus:shadow-blue-500 focus:border-blue-500'
-              />
-            </div>
-          )}
-          <Divider />
-
-          <div className='flex flex-col gap-3'>
-            <div>
-              <input
-                {...register('email')}
-                autoComplete='on'
-                placeholder='Correo electrónico'
-                className={`textbox h-[48px] ${!errors.email && 'focus:shadow-blue-500 focus:border-blue-500'} ${
-                  errors.email && 'border-red-500 shadow-red-500'
-                }`}
-              />
-              {errors.email && <p className='text-xs text-red-500 mt-1'>{errors.email?.message}</p>}
-            </div>
-            <div className='relative w-full'>
-              <input
-                {...register('password')}
-                autoComplete='on'
-                type={`${visiblePassword ? 'text' : 'password'}`}
-                placeholder='Contraseña'
-                className={`textbox h-[46px] ${
-                  !errors.password && 'focus:shadow-blue-500 focus:border-blue-500'
-                } pr-9 ${errors.password && 'border-red-500 shadow-red-500'}`}
-              />
-              <div className='absolute right-2 top-3' onClick={() => setVisiblePassword(!visiblePassword)}>
-                {visiblePassword ? <EyeSlashIcon /> : <Eye />}
+              <div className='relative w-full'>
+                <input
+                  {...register('password')}
+                  autoComplete='on'
+                  type={`${visiblePassword ? 'text' : 'password'}`}
+                  placeholder='Contraseña'
+                  className={`textbox h-[46px] ${
+                    !errors.password && 'focus:shadow-blue-500 focus:border-blue-500'
+                  } pr-9 ${errors.password && 'border-red-500 shadow-red-500'}`}
+                />
+                <div className='absolute right-2 top-3' onClick={() => setVisiblePassword(!visiblePassword)}>
+                  {visiblePassword ? <EyeSlashIcon /> : <Eye />}
+                </div>
+                {errors.password && <p className='text-xs text-red-500 mt-1'>{errors.password?.message}</p>}
               </div>
-              {errors.password && <p className='text-xs text-red-500 mt-1'>{errors.password?.message}</p>}
             </div>
+            {isLogin ? (
+              <Button
+                type='submit'
+                className='w-full shadow-md border border-gray-300 hover:border-[#FF8C00] hover:bg-none hover:text-[#FF8C00]'
+              >
+                <p className='text-lg text-center'>INICIAR SESIÓN</p>
+              </Button>
+            ) : (
+              <Button
+                type='submit'
+                variant='outline-primary'
+                className='w-full border border-[#FF8C00] shadow-md hover:bg-gradient-to-r from-[#FF8C00] to-[#FFD700]'
+              >
+                <p className='text-lg text-center'>CREAR CUENTA</p>
+              </Button>
+            )}
+          </form>
+          <div className='flex flex-col sm:flex-row justify-between items-center w-full px-5'>
+            {isLogin && <h3 className='text-blue-500 cursor-pointer'>¿Olvidó su contaseña?</h3>}
+            <h3 className=''>
+              {`${isLogin ? '¿No tenés cuenta?' : '¿Ya tenés cuenta?'}`}
+              <span className='ml-2 text-[#ff823f] cursor-pointer' onClick={() => setIsLogin(!isLogin)}>{`${
+                isLogin ? 'Registrate aquí' : 'Inicia sesión aquí'
+              }`}</span>
+            </h3>
           </div>
-          {isLogin ? (
-            <Button
-              type='submit'
-              className='w-full shadow-md border border-gray-300 hover:border-[#FF8C00] hover:bg-none hover:text-[#FF8C00]'
-            >
-              <p className='text-lg text-center'>INICIAR SESIÓN</p>
-            </Button>
-          ) : (
-            <Button
-              type='submit'
-              variant='outline-primary'
-              className='w-full border border-[#FF8C00] shadow-md hover:bg-gradient-to-r from-[#FF8C00] to-[#FFD700]'
-            >
-              <p className='text-lg text-center'>CREAR CUENTA</p>
-            </Button>
-          )}
-        </form>
-        <div className='flex justify-between items-center w-full px-5'>
-          {isLogin && <h3 className='text-blue-500 cursor-pointer'>¿Olvidó su contaseña?</h3>}
-          <h3 className=''>
-            {`${isLogin ? '¿No tenés cuenta?' : '¿Ya tenés cuenta?'}`}
-            <span className='ml-2 text-[#ff823f] cursor-pointer' onClick={() => setIsLogin(!isLogin)}>{`${
-              isLogin ? 'Registrate aquí' : 'Inicia sesión aquí'
-            }`}</span>
-          </h3>
         </div>
       </div>
     </div>
--- client/src/redux/slices/sliceUser.ts
+++ client/src/redux/slices/sliceUser.ts
@@ -4,20 +4,38 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 export interface Profile {
   name: string | null | undefined;
   email: string | null | undefined;
-  image: string | null | undefined;
+  img_avatar: string | null | undefined;
+  admin: boolean;
+  createdAt: string | null | undefined;
+  favoriteUsers: string[] | null | undefined;
+  favorites: string[] | null | undefined;
+  isOnline: boolean;
+  location: string | null | undefined;
+  posts: string[] | null | undefined;
+  savedPosts: string[] | null | undefined;
+  updatedAt: string | null | undefined;
+  _id: string | null | undefined;
 }
 export interface IUser {
-  authenticated?: boolean;
   user: Profile;
   token: string | null | undefined;
 }
 
 const initialState: IUser = {
-  authenticated: false,
   user: {
     name: null,
     email: null,
-    image: null,
+    img_avatar: null,
+    admin: false,
+    createdAt: null,
+    favoriteUsers: null,
+    favorites: null,
+    isOnline: false,
+    location: null,
+    posts: null,
+    savedPosts: null,
+    updatedAt: null,
+    _id: null,
   },
   token: null,
 };
@@ -27,7 +45,6 @@ export const user = createSlice({
   initialState,
   reducers: {
     setLogin: (state: IUser, { payload }: PayloadAction<IUser>) => {
-      state.authenticated = true;
       state.user = payload.user;
       state.token = payload.token;
     },
