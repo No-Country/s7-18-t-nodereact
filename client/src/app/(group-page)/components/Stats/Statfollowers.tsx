bb2672ebd43c24c629c320f49d8e627999ca9c60
Lucas Santamaria <83138881+LucasSantamaria76@users.noreply.github.com>
2023-04-16 17:59:00 -0300

    update new recipe (#54)
    Co-authored-by: 76luc <santamaria.luc76@gmail.com>

--- client/src/app/(group-page)/components/Stats/Statfollowers.tsx
+++ client/src/app/(group-page)/components/Stats/Statfollowers.tsx
@@ -1,11 +1,14 @@
 interface Props {
   textColor?: string;
+  followers: string[] | null | undefined;
 }
 
-const Statfollowers = ({ textColor = 'white' }: Props) => {
+const Statfollowers = ({ followers, textColor = 'white' }: Props) => {
   return (
     <div className='flex flex-col gap-1 p-0 w-20'>
-      <div className={`w-full h-6 text-xl text-center leading-6 font-medium text-${textColor}`}>15k</div>
+      <div className={`w-full h-6 text-xl text-center leading-6 font-medium text-${textColor}`}>
+        {followers?.length}
+      </div>
       <div className={`w-full h-5 text-sm text-center leading-4 font-light text-${textColor}`}>Seguidores</div>
     </div>
   );
