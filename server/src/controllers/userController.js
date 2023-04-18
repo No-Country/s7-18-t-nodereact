85f7d9167c1d1df1b1a55418e818be8ba0499469
KNBT <kimberlly.barraza.27@unsch.edu.pe>
2023-04-17 21:25:52 -0500

    EDIT: Corrección del error-al seguir nuevamente al mismo usuario, si bien aparecía el mensaje de que ya lo estaba siguiendo, mi ID aparecía por 2da vez en sus followers. Ahora está arreglado YA NO LO PERMITE.

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -146,11 +146,11 @@ const followUser = async (req, res) => {
     try {
 
         if (!userId || !userToFollowId) {
-            res.status(400).json({ message: "Debe proporcionar los IDs de usuario" });
+            return res.send({ message: "Debe proporcionar los IDs de usuario" });
         }
 
         if (userId === userToFollowId) {
-            res.status(400).json({ message: "No puedes seguirte a ti mismo" });
+            return res.send({ message: "No puedes seguirte a ti mismo" });
         }
 
         const user = await User.findById(userId);
@@ -158,11 +158,12 @@ const followUser = async (req, res) => {
 
 
         if (!user || !userToFollow) {
-            res.status(404).json({ message: "Usuario no encontrado" });
+            return res.send({ message: "Usuario no encontrado" });
         }
 
         if (user.following.includes(userToFollowId)) {
-            res.status(400).json({ message: "Ya sigues a este usuario" });
+            return res.send({ message: "Ya sigues a este usuario" });
+
         }
 
         user.following.push(userToFollowId);
