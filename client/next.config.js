f0b1f2cb1b950af0906405fb4b3f9952a853b9a6
Lucas Santamaria <83138881+LucasSantamaria76@users.noreply.github.com>
2023-04-18 14:44:20 -0300

    update new recipes (#68)
    Co-authored-by: 76luc <santamaria.luc76@gmail.com>

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['images.unsplash.com', 'www.gravatar.com', 'lh3.googleusercontent.com', 'ik.imagekit.io'],
+    domains: [
+      'images.unsplash.com',
+      'www.gravatar.com',
+      'lh3.googleusercontent.com',
+      'ik.imagekit.io',
+      'res.cloudinary.com',
+    ],
   },
 };
 
