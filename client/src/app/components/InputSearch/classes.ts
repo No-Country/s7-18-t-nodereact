08e2723ba3fa36311850576b60e42f203b9653ee
Lucas Santamaria <83138881+LucasSantamaria76@users.noreply.github.com>
2023-04-21 11:26:06 -0300

    Delete client directory

--- client/src/app/components/InputSearch/classes.ts
+++
@@ -1,6 +0,0 @@
-export const searchMobile = `
- flex gap-2 w-full border border-gray-200 rounded-lg py-2 px-1
-`;
-export const search = `
- hidden lg:flex gap-2 w-[400px] border border-gray-200 rounded-lg py-2 px-1
-`;
